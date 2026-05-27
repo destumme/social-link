aacc68229dd1aa4850f6a0e253b73fa3350f7024
destumme <destumme@gmail.com>
2026-05-26 20:14:28 -0500

    icon nonsense

--- src/components/layout/brand-icon.tsx
+++ src/components/layout/brand-icon.tsx
@@ -6,16 +6,27 @@ export default function BrandIcon({ className }: { className?: string }) {
       fill="none"
       className={className}
     >
-      {/* Bookmark outline */}
-      <path
-        d="M4 17.9808V9.70753C4 6.07416 4 4.25748 5.17157 3.12874C6.34315 2 8.22876 2 12 2C15.7712 2 17.6569 2 18.8284 3.12874C20 4.25748 20 6.07416 20 9.70753V17.9808C20 20.2867 20 21.4396 19.2272 21.8523C17.7305 22.6514 14.9232 19.9852 13.59 19.1824C12.8168 18.7168 12.4302 18.484 12 18.484C11.5698 18.484 11.1832 18.7168 10.41 19.1824C9.0768 19.9852 6.26947 22.6514 4.77285 21.8523C4 21.4396 4 20.2867 4 17.9808Z"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="1.15"
+      {/* Background rounded square */}
+      <rect
+        x="1"
+        y="0.5"
+        width="20"
+        height="21"
+        rx="4"
+        className="fill-secondary"
       />
+      {/* Bookmark outline */}
+      <g transform="translate(1,1) scale(0.80)">
+        <path
+          d="M4 17.9808V9.70753C4 6.07416 4 4.25748 5.17157 3.12874C6.34315 2 8.22876 2 12 2C15.7712 2 17.6569 2 18.8284 3.12874C20 4.25748 20 6.07416 20 9.70753V17.9808C20 20.2867 20 21.4396 19.2272 21.8523C17.7305 22.6514 14.9232 19.9852 13.59 19.1824C12.8168 18.7168 12.4302 18.484 12 18.484C11.5698 18.484 11.1832 18.7168 10.41 19.1824C9.0768 19.9852 6.26947 22.6514 4.77285 21.8523C4 21.4396 4 20.2867 4 17.9808Z"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1"
+        />
+      </g>
       {/* Smile Plus - scaled and centered inside bookmark */}
-      <g transform="translate(7, 7) scale(0.40)" strokeWidth="2.5">
+      <g transform="translate(7, 6) scale(0.30)" strokeWidth="2.25">
         <path
           d="M21.3 10.5C21.4311 11.1462 21.5 11.8151 21.5 12.5C21.5 18.0228 17.0228 22.5 11.5 22.5C5.97715 22.5 1.5 18.0228 1.5 12.5C1.5 6.97715 5.97715 2.5 11.5 2.5C12.1849 2.5 12.8538 2.56886 13.5 2.70004"
           stroke="currentColor"
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 notebook-vertical-line relative">
       <div className="w-full flex h-24 items-center gap-6 px-0 lg:px-2">
-        <Link href="/" className="pl-0">
+        <Link href="/" className="pl-0 text-tertiary">
           <BrandIcon className="w-20 h-20" />
         </Link>
 
